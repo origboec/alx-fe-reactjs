463d48843037f49cdd66f298ed4231d60b2d86a0
Emmanuel Origbo <origboec@gmail.com>
2025-08-28 09:52:33 +0100

    Add React app created with Vite and React template

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Emmanuel 👋</h1>
+      <p>Welcome to your first React App built with Vite!</p>
+    </div>
+  );
+}
+
+export default App;
