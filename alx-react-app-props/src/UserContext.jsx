cbcc181787bce19488df1682630f3f04e639302f
Emmanuel Origbo <origboec@gmail.com>
2025-09-14 19:01:06 +0100

    Final Fix: Updated UserProfile.jsx to meet checker requirement

--- alx-react-app-props/src/UserContext.jsx
+++ alx-react-app-props/src/UserContext.jsx
@@ -1,9 +1,7 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext } from 'react';
 
-// Step 1: Create the context
 const UserContext = createContext();
 
-// Step 2: Create the provider component
 export const UserProvider = ({ children }) => {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
@@ -14,5 +12,4 @@ export const UserProvider = ({ children }) => {
   );
 };
 
-// Step 3: Create a custom hook for consuming the context
 export const useUser = () => useContext(UserContext);
--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -7,3 +7,4 @@ function UserInfo() {
 export default UserInfo;
 
 
+
