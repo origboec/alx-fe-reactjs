a5c1a41e3454b79b7d96c018c7861b4f0c9d340f
Emmanuel Origbo <origboec@gmail.com>
2025-09-21 18:42:02 +0100

    Fix routing: single Router in main.jsx, update App.jsx with Routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/add">Add Recipe</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<RecipeList />} />
-          <Route path="/add" element={<AddRecipeForm />} />
-          <Route path="/recipes/:id" element={<RecipeDetails />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/add">Add Recipe</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
+
 
 
 
