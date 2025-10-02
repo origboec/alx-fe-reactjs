697230588bf1a572aadf47e3f0ba522004274257
Emmanuel Origbo <origboec@gmail.com>
2025-10-02 22:01:41 +0100

    Task 1: Styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,12 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is working 🎉
-      </h1>
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <UserProfile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
